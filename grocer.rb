b58d6264358a2716576a1d0b2b235976fd61f317
angelica winkler <caw400@nyu.edu>
2018-08-11 15:38:35 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,109 @@
+require "pry"
+
 def consolidate_cart(cart)
-  # code here
+  newHash = {}
+
+  cart.each do |item, item_info|
+    item.each do |food, food_facts|
+      newHash[food] = {}
+    end
+  end
+  newHash
+
+  cart.each do |item, item_info|
+    item.each do |food, food_facts|
+      food_facts.each do |key, value|
+        newHash[food][key] = value
+        newHash[food][:count] = 0
+      end
+    end
+  end
+  newHash
+  cart.each do |item, item_info|
+    item.each do |food, food_facts|
+      if newHash.include?(food)
+        newHash[food][:count] += 1
+      end
+    end
+  end
+  newHash
 end
+
 
 def apply_coupons(cart, coupons)
-  # code here
+  newCart = cart
+
+  coupons.each do |info, coupon_hashes|
+    info.each do |key, value|
+      if cart.keys.include?(info[:item])
+
+        items_required = info[:num]
+        items_in_cart = cart[info[:item]].fetch(:count)
+
+        if items_in_cart >= items_required
+          coupon_deals = (items_in_cart / items_required).floor
+          remainder = items_in_cart % items_required
+          food_name = info[:item]
+          discounted_items = "#{food_name} W/COUPON"
+          newCart[discounted_items] = {:price => info[:cost], :clearance => cart[food_name][:clearance], :count => coupon_deals}
+          newCart[info[:item]][:count] = remainder
+        end
+      end
+    end
+  end
+  newCart
 end
+
+
+
+
+
+
+
 
 def apply_clearance(cart)
-  # code here
+  newCart = {}
+
+  cart.each do |item, item_info|
+    item_info.each do |key, value|
+      if item_info[:clearance] == false
+        newCart[item] = item_info
+      elsif item_info[:clearance] == true
+
+        discounted_price = item_info[:price] * 0.8
+        newCart[item] = {:price => discounted_price.round(2), :clearance => true, :count => cart[item][:count]}
+
+      end
+    end
+  end
+  newCart
 end
 
+
+
 def checkout(cart, coupons)
-  # code here
+  cart_summary = consolidate_cart(cart)
+  cart_with_coupons = apply_coupons(cart_summary, coupons)
+  cart_with_clearance = apply_clearance(cart_with_coupons)
+
+  prices = []
+
+  cart_with_clearance.each do |item, item_info|
+    item_info.each do |key, value|
+      prices << item_info[:price]
+    end
+  end
+
+  prices
+  sum = 0
+  i = 0
+
+  while i < prices.length
+    sum += prices[i]
+    i += 1
+  end
+  sum
+  if sum > 100
+    discount_sum = sum * 0.9
+  binding.pry
 end
